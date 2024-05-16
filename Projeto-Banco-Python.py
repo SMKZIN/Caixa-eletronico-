e6b0017aae6959891ee6a40dba86526fa813b927
SMKZIN <67834597+SMKZIN@users.noreply.github.com>
2024-05-16 17:39:58 -0300

    Update Projeto-Banco-Python.py

--- Projeto-Banco-Python.py
+++ Projeto-Banco-Python.py
@@ -1,8 +1,11 @@
 # --- tela inicial ---
-deposito = float
-saldo = float
-SAQUE_MAXIMO = 500
-LIMITE_DE_SAQUES = 3
+extrato = True
+deposito = True
+saldo = 0
+saque = float
+MAX_SAQUES_POR_DIA = 3
+VALOR_MAXIMO_SAQUE = 500
+saques_feitos= 0
 
 print("""         -------- Banco XI --------
       
@@ -42,24 +45,55 @@ elif OPCAO == 2:
 elif OPCAO == 3:
     print("Exibindo extrato bancário...")
 
-# --- Saque,deposito e extrato --- 
-destinatario = str
+# --- Deposito --- 
+
 if OPCAO == 1:
-    float(input("Insira o valor do depósito:"))
+    deposito = float(input("Insira o valor do depósito:"))
     destinatario = (input("Destino do depósito:"))
-    str(input("Você tem certeza?"))
-    print(f"Seu depósito para {destinatario}, foi realizado com sucesso !")
-    while deposito :
-        
+    confirmaçao = (input("Você tem certeza? (sim/não):"))
+    if confirmaçao.lower() == "sim":
+        saldo += deposito  
+        print(f"Seu depósito para {destinatario} no valor de R$ {deposito}, foi realizado com sucesso !")
+        print(f"Saldo atual : {saldo}")
+    else:
+        print("Operação cancelada.")
+     
+ # --- saque ---
+       
 elif OPCAO == 2:
-    float(input("Insira o valor do saque:")) 
-    input("Voce tem certeza?")
-    print("Sacando...")
-    while SAQUE_MAXIMO >= 500:
-       print("ERROR : Infelizmente, so podemos sacar ate R$ 500 diariamente. ")
-       break
+    # Verificar se já excedeu o número máximo de saques permitidos
+    if saques_feitos >= MAX_SAQUES_POR_DIA:
+        print("Você já atingiu o limite de saques para hoje.")
+    else:
+        # Solicitar o valor do saque
+        saque = float(input("Insira o valor do saque:")) 
+        # Verificar se o valor do saque excede o limite máximo
+        if saque > VALOR_MAXIMO_SAQUE:
+            print(f"O valor máximo de saque é de R$ {VALOR_MAXIMO_SAQUE}.")
+        else:
+            confirmacao = input("Você tem certeza? (sim/não): ")
+            if confirmacao.lower() == "sim":
+                # Atualizar o número de saques feitos no dia
+                saques_feitos += 1
+                print("Sacando...")
+            else:
+                print("Operação cancelada.")
+ # --- Extrato --- 
 
+while extrato:
+    
+    print(f"""    -------- Banco XI -------- 
+          
+          // EXTRATO //
+             
+    - Saldo em Conta : {saldo}
+    - Conta : {login}
+    - Naturalidade : Brasil, Minas Gerais                
+    - Data : 12/04/2024
+    - Hora : 12:14
+    --------------------------
 
-
-  
-
+    Obrigado pela preferência :)
+          """)
+    break
+    
